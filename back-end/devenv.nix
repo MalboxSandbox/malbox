700cf2686ae535bdc616e8141c6b1333b4dc28c8
shard7 <sh7user@gmail.com>
2026-04-23 21:06:02 +0200

    fix(devenv): move back-end specific configuration to respective devenv.nix file

--- back-end/devenv.nix
+++ back-end/devenv.nix
@@ -46,6 +46,27 @@ in {
     MINGW_INCLUDE_PATH = "${mingwMcfgthreads.dev}/include";
   };
 
+  languages = {
+    rust = {
+      enable = true;
+      channel = "stable";
+      components = [ "rustc" "cargo" "clippy" "rustfmt" "rust-analyzer" ];
+      targets = [ "x86_64-pc-windows-gnu" "x86_64-unknown-linux-gnu" ];
+    };
+    python.enable = true;
+    perl.enable = true;
+  };
+
+  git-hooks.hooks = {
+    cargo-check.enable = true;
+    clippy.enable = true;
+    rustfmt.enable = true;
+  };
+
+  git-hooks.settings = {
+    rust.cargoManifestPath = "./back-end/Cargo.toml";
+  };
+
   packages = with pkgs; [
     nixd
     sqlx-cli
--- devenv.nix
+++ devenv.nix
@@ -1,24 +1,4 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
-  languages = {
-    rust = {
-      enable = true;
-      channel = "stable";
-      components = [ "rustc" "cargo" "clippy" "rustfmt" "rust-analyzer" ];
-      targets = [ "x86_64-pc-windows-gnu" "x86_64-unknown-linux-gnu" ];
-    };
-    python.enable = true;
-    perl.enable = true;
-  };
-
-  git-hooks.hooks = {
-    cargo-check.enable = true;
-    clippy.enable = true;
-    rustfmt.enable = true;
-  };
-
-  git-hooks.settings = {
-    rust.cargoManifestPath = "./back-end/Cargo.toml";
-  };
 }
