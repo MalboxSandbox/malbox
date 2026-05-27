79fed2a69d281e7d7058cd48c8fe67f4a2736a60
shard7 <sh7user@gmail.com>
2026-05-27 15:05:57 +0200

    feat(front-end): add navigation progress bar to root layout

--- front-end/src/routes/+layout.svelte
+++ front-end/src/routes/+layout.svelte
@@ -2,12 +2,12 @@
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import { auth } from '$lib/stores/auth.svelte';
+	import { navigating } from '$app/stores';
 	import { onMount } from 'svelte';
 	import Toasts from '$lib/components/Toasts.svelte';
 
 	let { children } = $props();
 
-	// Check auth status on app load
 	onMount(() => {
 		auth.checkAuth();
 	});
@@ -17,6 +17,32 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+{#if $navigating}
+	<div class="fixed left-0 top-0 z-50 h-0.5 w-full overflow-hidden bg-[var(--color-bg-tertiary)]">
+		<div class="nav-progress h-full bg-[var(--color-accent)]"></div>
+	</div>
+{/if}
+
 {@render children?.()}
 
 <Toasts />
+
+<style>
+	.nav-progress {
+		animation: nav-slide 2s ease-in-out infinite;
+	}
+	@keyframes nav-slide {
+		0% {
+			width: 0%;
+			margin-left: 0%;
+		}
+		50% {
+			width: 40%;
+			margin-left: 30%;
+		}
+		100% {
+			width: 0%;
+			margin-left: 100%;
+		}
+	}
+</style>
