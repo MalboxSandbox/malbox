01d7eea29042f9d5bcd8f51b51ca1eceb622b4a1
shard7 <sh7user@gmail.com>
2026-05-27 15:02:55 +0200

    refactor(front-end): remove standalone images and machines pages

--- front-end/src/routes/(app)/images/+page.ts
+++
@@ -1,8 +0,0 @@
-import { listImages } from '$lib/api/images';
-import type { PageLoad } from './$types';
-
-export const load: PageLoad = async ({ fetch, depends }) => {
-	depends('malbox:images');
-	const images = await listImages(fetch);
-	return { images };
-};
--- front-end/src/routes/(app)/machines/+page.ts
+++
@@ -1,8 +0,0 @@
-import { listMachines } from '$lib/api/machines';
-import type { PageLoad } from './$types';
-
-export const load: PageLoad = async ({ fetch, depends }) => {
-	depends('malbox:machines');
-	const machines = await listMachines(fetch);
-	return { machines };
-};
